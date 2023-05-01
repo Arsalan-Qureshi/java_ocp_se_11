35093972f22fd349005a56c774d8f6c1aad6e30b
Arsalan Ahmed Qureshi <thearsalanahmedqureshi@gmail.com>
2023-05-02 02:20:58 +0500

    Added details related to abstract classes for CHP 9

--- CHP 9/pkg/Abs.java
+++ CHP 9/pkg/Abs.java
@@ -1,13 +1,30 @@
-public class Abs extends Bird {
+package pkg;
+
+public class Abs extends Bird {	
 	public String getName(){ return "HO-HO"; }
 	
 	public static void main(String... args){
-		System.out.println(new Abs().getName());
+		var b = new Abs();
+		/* Bird is abstract; cannot be instantiated
+		var b2 = new Bird(); */
+		System.out.println(b.getName() + " - " + b.numberOfTalons);
 	}
 }
 
-/* Class that cannot be intialized and may contain abstract methods. */
+/* Class that cannot be intialized and may contain abstract methods. 
+	- A static method cannot be overridden but only hidden, which means it also can not be marked as abstract.
+	- Top-level abstract classes can't be marked protected or private.
+	- No abstract class can be marked final.
+*/
 abstract class Bird {
+	int numberOfTalons;
+	
+	// Abstract class can contain a constructor that is used for initialization
+	// but cannot be called directly.
+	public Bird(){
+		numberOfTalons = 3;
+	}	
+	
 	public abstract String getName();
 }
 
